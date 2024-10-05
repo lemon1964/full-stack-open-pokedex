2c47c1727944e0faa041ab3bef629b4787a314fa
lemon <lemon@example.com>
2024-10-05 16:17:34 +0300

    Deploy Health

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 
-// app.get('/health', (req, res) => {
-//   res.send('Everything is fine')
-// })
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
-  res.send('ok')
+  res.send('Health is fine!')
 })
+// app.get('/health', (req, res) => {
+//   // eslint-disable-next-line no-constant-condition
+//   if (true) throw('error...  ')
+//   res.send('ok')
+// })
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
